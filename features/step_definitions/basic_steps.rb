d2ef5a07234ac955cbd4b2cd0d9bc11fdb84ba91
hanna_nyman <hannanyman.sk@gmail.com>
2019-03-05 21:51:52 +0100

    Writes step definitions

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,24 @@
+Given("the following Admin exists:") do |table|
+    table.hashes.each do |admin|
+        FactoryBot.create(:admin, admin)
+    end
+end
+
+Given("I am logged in as Admin") do
+    visit new_admin_session_path
+    fill_in 'admin[email]', with: 'admin@mail.com'
+    fill_in 'admin[password]', with: 'password'
+    find('#login').click
+end
+
+Given("I visit the menu page") do
+    visit menu_index_path
+end
+
+Given("I click on {string}") do |string|
+    click_on string
+end
+
+Given("I fill in {string} with {string}") do |field, value|
+    fill_in field, with: value
+end
