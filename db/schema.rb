17f475f1b8cb9861654118dfbfb19207c4740aca
hanna_nyman <hannanyman.sk@gmail.com>
2019-03-05 20:45:56 +0100

    Adds migrations to items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_162807) do
+ActiveRecord::Schema.define(version: 2019_03_05_194447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2019_03_05_162807) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "description"
+    t.string "price"
+    t.string "category"
   end
 
 end
